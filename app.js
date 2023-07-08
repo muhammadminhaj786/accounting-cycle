a117f26815032917fc2c83168661c8cee313986c
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-07-08 03:38:39 -0700

    Update app.js

--- app.js
+++ app.js
@@ -52,9 +52,74 @@ function record(){
         Damount,
         Dtype
     }
-    localStorage.setItem("Dobj",JSON.stringify(dObj))
-    console.log(dObj)
+    var getDObj = JSON.parse(localStorage.getItem('debit'))
+    if (getDObj === null){
+        var arr = []
+        arr.push(dObj)
+        localStorage.setItem('debit',JSON.stringify(arr))
+        // console.log(arr)
+    }else{
+        getDObj.push(dObj)
+        localStorage.setItem('debit',JSON.stringify(getDObj))
+    }
+
+    //creating a credit object
+    var dObj = {
+        Cinfo,
+        Camount,
+        Ctype
+        }
+    var getCObj = JSON.parse(localStorage.getItem('credit'))
+    if (getCObj === null){
+        var arr = []
+        arr.push(dObj)
+        localStorage.setItem('credit',JSON.stringify(arr))
+        // console.log(arr)
+    }else{
+        getCObj.push(dObj)
+        localStorage.setItem('credit',JSON.stringify(getCObj))
+    }
 
 }
 
 //working on generalEntries page
+var tbody = document.querySelector('.tbody');
+
+//create event listner
+window.addEventListener('load',function(){
+    var myarr = []
+    var getDObj = JSON.parse(this.localStorage.getItem('debit'))
+    var getcObj = JSON.parse(this.localStorage.getItem('credit'))
+    myarr.push(getDObj)
+    myarr.push(getcObj)
+    console.log(myarr)
+    // for (const i of getObj) {
+    //     console.log(i.Dinfo)
+    //     var tableBody = `             <tr>
+    //     <th scope="row">1</th>
+    //     <td>${i.Dinfo}</td>
+    //     <td>${i.Damount}</td>
+    //     <td>${i.Dtype}</td>
+    //     </tr>
+    //     <tr>
+    //     <th scope="row">1</th>
+    //       <td>${i.Cinfo}</td>
+    //       <td>${i.Camount}</td>
+    //       <td>${i.Ctype}</td>
+    //     </tr>
+    //     `
+    //     tbody.innerHTML += tableBody
+    
+    // }
+    // for (const i of getCObj) {
+    //     var tableBody = `            
+        
+    //     <tr>
+    //     <th scope="row">1</th>
+    //       <td>${i.Cinfo}</td>
+    //       <td>${i.Camount}</td>
+    //       <td>${i.Ctype}</td>
+    //     </tr>`
+    //     tbody.innerHTML += tableBody
+    
+    // }
