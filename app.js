7206202818f2ffbf14193a27e16bfdc0a2025908
Muhammad Minhaj Wahid <122244089+muhammadminhaj786@users.noreply.github.com>
2023-07-09 00:05:00 -0700

    GE and trial balance done

--- app.js
+++ app.js
@@ -46,39 +46,59 @@ function record(){
     var Dtype = document.getElementById('typeA').value
     var Ctype = document.getElementById('typeB').value
 
-    //creating a debit object
-    var dObj = {
+//     //creating a debit object
+//     var dObj = {
+//         Dinfo,
+//         Damount,
+//         Dtype
+//     }
+//     var getDObj = JSON.parse(localStorage.getItem('debit'))
+//     if (getDObj === null){
+//         var arr = []
+//         arr.push(dObj)
+//         localStorage.setItem('debit',JSON.stringify(arr))
+//         // console.log(arr)
+//     }else{
+//         getDObj.push(dObj)
+//         localStorage.setItem('debit',JSON.stringify(getDObj))
+//     }
+
+//     //creating a credit object
+//     var CObj = {
+//         Cinfo,
+//         Camount,
+//         Ctype
+//         }
+//     var getCObj = JSON.parse(localStorage.getItem('credit'))
+//     if (getCObj === null){
+//         var arr = []
+//         arr.push(dObj)
+//         localStorage.setItem('credit',JSON.stringify(arr))
+//         // console.log(arr)
+//     }else{
+//         getCObj.push(dObj)
+//         localStorage.setItem('credit',JSON.stringify(getCObj))
+//     }
+
+    var Obj = {
         Dinfo,
         Damount,
-        Dtype
-    }
-    var getDObj = JSON.parse(localStorage.getItem('debit'))
-    if (getDObj === null){
-        var arr = []
-        arr.push(dObj)
-        localStorage.setItem('debit',JSON.stringify(arr))
-        // console.log(arr)
-    }else{
-        getDObj.push(dObj)
-        localStorage.setItem('debit',JSON.stringify(getDObj))
-    }
-
-    //creating a credit object
-    var dObj = {
+        Dtype,
         Cinfo,
         Camount,
         Ctype
-        }
-    var getCObj = JSON.parse(localStorage.getItem('credit'))
-    if (getCObj === null){
+    }
+    var getObj = JSON.parse(localStorage.getItem('accounts'))
+    if (getObj === null){
         var arr = []
-        arr.push(dObj)
-        localStorage.setItem('credit',JSON.stringify(arr))
+        arr.push(Obj)
+        localStorage.setItem('accounts',JSON.stringify(arr))
         // console.log(arr)
     }else{
-        getCObj.push(dObj)
-        localStorage.setItem('credit',JSON.stringify(getCObj))
+        getObj.push(Obj)
+        localStorage.setItem('accounts',JSON.stringify(getObj))
     }
+
 
 }
 
@@ -87,39 +107,41 @@ var tbody = document.querySelector('.tbody');
 
 //create event listner
 window.addEventListener('load',function(){
-    var myarr = []
-    var getDObj = JSON.parse(this.localStorage.getItem('debit'))
-    var getcObj = JSON.parse(this.localStorage.getItem('credit'))
-    myarr.push(getDObj)
-    myarr.push(getcObj)
-    console.log(myarr)
-    // for (const i of getObj) {
-    //     console.log(i.Dinfo)
-    //     var tableBody = `             <tr>
-    //     <th scope="row">1</th>
-    //     <td>${i.Dinfo}</td>
-    //     <td>${i.Damount}</td>
-    //     <td>${i.Dtype}</td>
-    //     </tr>
-    //     <tr>
-    //     <th scope="row">1</th>
-    //       <td>${i.Cinfo}</td>
-    //       <td>${i.Camount}</td>
-    //       <td>${i.Ctype}</td>
-    //     </tr>
-    //     `
-    //     tbody.innerHTML += tableBody
+    var getObj = JSON.parse(this.localStorage.getItem('accounts'))
+    for (const i of getObj) {
+        var id = 1;
+        var tableBody = `             <tr>
+        <th scope="row">${id++}</th>
+        <td>${i.Dinfo}</td>
+        <td>${i.Damount}</td>
+        <td>${"-"}</td>
+        </tr>
+        <tr>
+        <th scope="row">${id++}</th>
+          <td>${i.Cinfo}</td>
+          <td>${"-"}</td>
+          <td>${i.Camount}</td>
+        </tr>
+        `
+        tbody.innerHTML += tableBody
     
-    // }
-    // for (const i of getCObj) {
-    //     var tableBody = `            
-        
-    //     <tr>
-    //     <th scope="row">1</th>
-    //       <td>${i.Cinfo}</td>
-    //       <td>${i.Camount}</td>
-    //       <td>${i.Ctype}</td>
-    //     </tr>`
-    //     tbody.innerHTML += tableBody
-    
-    // }
+    }
+
+})
+
+//working on trail balance page
+window.addEventListener('load',function(){
+    var trailbody = document.querySelector('.trialbody');
+    var getObj = JSON.parse(this.localStorage.getItem('accounts'));
+    var id = 1;
+    for (const i of getObj) {
+        console.log(i.Camount)
+        var trailDiv = `
+        <tr>
+        <td>${i.Dinfo}</td>
+        <td>${i.Damount}</td>
+        <td>${i.Camount}</td>
+        </tr>`
+        trailbody.innerHTML += trailDiv
+    }
+})
